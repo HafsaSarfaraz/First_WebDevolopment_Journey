36db4ec44fc404191b4a0178c5377ab7c4c5d0d0
Hafsa Sarfaraz <154260956+HafsaSarfaraz@users.noreply.github.com>
2024-08-24 19:58:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let H1= document.querySelector("h1");
+console.dir(H1.innerText);
+
+H1.innerText= H1.innerText + " From Apna College";  //concatenate two strings
+
+let div= document.querySelectorAll(".box");
+div[0].innerText= "my name is hafsa";
+div[1].innerText= "I am 20 years old";
+div[2].innerText= "I am in BS-SE";
